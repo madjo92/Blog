1572aed31ce770b4263a1faf16187253d56efd82
unknown <viktor_madzovski@hotmail.com>
2021-04-22 22:57:15 +0200

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from posts.views import index , blog, post_detail, search, post_update, post_delete, post_create
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    path('accounts/', include('allauth.urls')),
+    path('blog/', blog, name='post-list'),
+    path('post/<id>/', post_detail, name='post-detail'),
+    path('create/', post_create, name='post-create'),
+    path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<id>/delete/', post_delete, name='post-delete'),
+    path('search/', search , name='search'),
+    path('tinymce/', include('tinymce.urls'))
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
